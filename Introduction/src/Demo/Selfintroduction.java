dfc3297db98879e040b396e7a26729d87bc269ec
partnerrajendra <rajendra.ar@QT-INBG-LT1389.QUALITEST.LOCAL>
2024-06-21 19:33:17 +0530

    Commiting project

---
+++ Introduction/src/Demo/Selfintroduction.java
@@ -0,0 +1,16 @@
+package Demo;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selfintroduction {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://toolsqa.com/selenium-webdriver/run-selenium-test-script/");
+		
+
+	}
+
+}
