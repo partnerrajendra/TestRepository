1f04993f5c4bad821111da6323475c39f26d7238
320258581 <320258581@INGBTCPIC6VWM41.code1.emi.philips.com>
2024-07-12 13:03:30 +0530

    Added Waits

--- Introduction/src/Demo/TestDemo.java
+++ Introduction/src/Demo/TestDemo.java
@@ -1,17 +1,27 @@
 package Demo;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class TestDemo {
 
-	public static void main(String[] args) {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws Exception {
 
 		WebDriver driver = new ChromeDriver();
-		
-		driver.get("https://toolsqa.com/selenium-webdriver/run-selenium-test-script/");
-	    driver.manage().window().maximize();
-		
-
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(3000);
+	    driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("admin");
+	    driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+	    Thread.sleep(6000);
+	    driver.findElement(By.xpath("//div[@class='oxd-form-actions orangehrm-login-action']//button[contains(@type,'submit')]")).click();
+	    Thread.sleep(3000);
+	    driver.close();
 	}
 
 }
